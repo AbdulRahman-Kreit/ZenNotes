499fbce4a9ab8e2b83bf1318fc865057b927dfd6
AbdulRahman-Kreit <aleppianeren@gmail.com>
2026-01-01 10:08:47 +0200

    ENV variable has been modified

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import process from 'process';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
-  base: '/ZenNotes',
+  base: process.env.VITE_BASE_PATH || '/ZenNotes',
 })
